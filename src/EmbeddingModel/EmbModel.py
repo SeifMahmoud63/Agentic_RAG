2773b09d8e9be45ed5e4ba72438a6b2ec2a8e0fd
Seif Mahmoud <seifmahmoud7199.com>
2026-04-17 15:09:01 +0200

    edit in embedding file

--- src/EmbeddingModel/EmbModel.py
+++ src/EmbeddingModel/EmbModel.py
@@ -1,13 +1,9 @@
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from functools import lru_cache
+from helpers import config
 
 @lru_cache(maxsize=1)
 def get_embedding():
-    """
-    Returns the HuggingFace BGE embedding model.
-    Model: sentence-transformers/all-MiniLM-L6-v2
-    Dimension: 384
-    """
     return HuggingFaceBgeEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name=config.get_settings().EMBEDDING_MODEL_NAME
     )
