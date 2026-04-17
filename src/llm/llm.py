afe053d5faaaecb357966333dabd13973fee3d76
Seif Mahmoud <seifmahmoud7199.com>
2026-04-17 15:17:36 +0200

    edit in llm file

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -1,8 +1,11 @@
 from langchain_cohere import ChatCohere
 from helpers.config import get_settings
+from functools import lru_cache
 
+@lru_cache(maxsize=1)
 def get_llm():
+    settings = get_settings()
     return ChatCohere(
-        model=get_settings().MODEL_NAME,
-        api_key=get_settings().COHERE_API_KEY
+        model=settings.MODEL_NAME,
+        api_key=settings.COHERE_API_KEY
     )
