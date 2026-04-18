cf6f7aa9f341cd6ff958dcb41cb0e9df27b26e75
Seif Mahmoud <seifmahmoud7199.com>
2026-04-19 00:10:09 +0200

    edit in the logs

--- src/logs/logger.py
+++ src/logs/logger.py
@@ -6,7 +6,6 @@ def setup_logger(name="Agentic-RAG"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    # Clear existing handlers if any to avoid duplication
     if logger.hasHandlers():
         logger.handlers.clear()
 
@@ -15,21 +14,17 @@ def setup_logger(name="Agentic-RAG"):
     )
 
     ch = logging.StreamHandler()
-    # handlers just deliver the log where ? stream -> in CMD
     ch.setFormatter(formatter)
 
-    # Get the logs directory relative to this script
     log_dir = os.path.dirname(os.path.abspath(__file__))
     log_file_path = os.path.join(log_dir, "Agentic-RAG.log")
 
     fh = logging.FileHandler(log_file_path, encoding='utf-8')
-    # deliver the logs in file 
     fh.setFormatter(formatter)
 
     logger.addHandler(ch)
     logger.addHandler(fh)
 
-    # Disable propagation to avoid double logging 
     logger.propagate = False
 
     return logger
